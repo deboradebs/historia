19ef900cc37eb79307e4bd5b9956059840d44f43
naudebs <debora.alves04@escola.pr.gov.br>
2026-08-20 09:32:17 -0300

    j

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ function mostraPergunta(){
         return
     }
     perguntaAtual = perguntas[atual] 
-    caixaPerguntas.textContent =perguntaAtual.enunciado
+    caixaPerguntas.textContent = perguntaAtual.enunciado
 
 
 
